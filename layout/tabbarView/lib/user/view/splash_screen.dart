3d027cbfbdcc712b645e5886b84730d0d181e16e
Json Jeon <jsk005@naver.com>
2023-11-18 09:11:39 +0900

    tabbarView Layout Sample

---
+++ layout/tabbarView/lib/user/view/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:rest/common/const/colors.dart';
+import 'package:rest/common/layout/default_layout.dart';
+import 'package:rest/common/const/data.dart';
+import 'package:rest/user/view/login_screen.dart';
+
+import '../../common/view/root_tab.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    checkToken();
+  }
+
+  void checkToken() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    if (refreshToken == null || accessToken == null) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (_) => LoginScreen(),
+          ),
+          (route) => false);
+    } else {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (_) => RootTab(),
+          ),
+          (route) => false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      backgroundColor: PRIMARY_COLOR,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/img/logo/logo.png',
+              width: MediaQuery.of(context).size.width / 2,
+            ),
+            const SizedBox(height: 16.0),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
